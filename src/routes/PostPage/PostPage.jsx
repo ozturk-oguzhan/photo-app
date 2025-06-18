1211111139340f8c645f1cd5858a244f5cab91ed
Oğuzhan Öztürk <76633114+ozturk-oguzhan@users.noreply.github.com>
2025-06-18 23:10:38 +0300

    init single post

--- src/routes/PostPage/PostPage.jsx
+++ src/routes/PostPage/PostPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import "./PostPage.css";
+import { Link } from "react-router";
 const PostPage = () => {
-  return <div>PostPage</div>;
+  return (
+    <div className="post-page">
+      <img src="/general/arrow.svg" alt="" className="back" />
+      <div className="post-container">
+        <div className="post-image">
+          <img src="/pins/pin1.jpeg" alt="" />
+        </div>
+        <div className="post-detail">
+          <Link to="/john" className="user-detail">
+            <img src="/general/noAvatar.png" alt="" />
+            <span>John Doe</span>
+          </Link>
+          <span>Comments</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostPage;
